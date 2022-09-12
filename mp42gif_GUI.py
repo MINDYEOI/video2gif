9b352d19deb51d7717c2b4625969cde6b5c566c2
MINDYEOI <mindyeoi@khu.ac.kr>
2022-09-13 01:24:03 +0900

    [feat] Add GUI Code

---
+++ mp42gif_GUI.py
@@ -0,0 +1,74 @@
+# https://wikidocs.net/21928
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+from moviepy.editor import *
+import argparse
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        # Geometry
+        self.setWindowTitle('Video2GIF')
+        # self.setWindowIcon(QIcon('./assets/logo.png'))
+        self.setGeometry(300, 300, 300, 200)
+
+        # self.bFileName = False
+
+        # Load data button
+        # ref) https://newbie-developer.tistory.com/122, https://dotsnlines.tistory.com/501
+        self.pushButton = QPushButton('Open File')
+        self.pushButton.clicked.connect(self.pushButtonClicked)
+
+        self.label = QLabel()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.pushButton)
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+    def pushButtonClicked(self):
+
+        self.fileName = QFileDialog.getOpenFileName(
+            self, "open file", "./", "mp4 file(*.mp4) ;; avi file(*.avi)")
+
+        if self.fileName[0]:
+            self.label.setText("Converting...")
+
+            inputName = self.fileName[0]
+            outputName = inputName[:-4] + '.gif'
+            print(inputName)
+            print(outputName)
+            videoClip = VideoFileClip(inputName)
+            videoClip.write_gif(outputName)
+            # self.label.setText(" ")
+            self.label.setText("Converted!")
+
+        else:
+            self.label.setText("No file selected")
+
+    # def convert(self):
+    #     inputName = self.fileName[0]
+    #     outputName = inputName[:-4] + '.gif'
+    #     print(inputName)
+    #     print(outputName)
+    #     videoClip = VideoFileClip(inputName)
+    #     videoClip.write_gif(outputName)
+    #     self.label.setText(" ")
+    #     self.convertedLabel.setText("Converted!")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    # print(str(ex.label.isVisible()))
+    ex.show()
+    sys.exit(app.exec_())
